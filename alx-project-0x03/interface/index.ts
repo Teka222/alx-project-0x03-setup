d6a8d923f6786c5134b97cf9833d90a813ba7542
Tekano Meso <tekanonetflorist01@gmail.com>
2025-08-17 15:26:41 +0200

    Create index.ts

---
+++ alx-project-0x03/interface/index.ts
@@ -0,0 +1,16 @@
+export interface ButtonProps {
+  buttonLabel: string;
+  buttonSize?: string;
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green';
+  action?: () => void;
+}
+
+import { ReactNode } from 'react';
+export interface LayoutProps {
+  children: ReactNode;
+}
+
+export interface PillProps {
+  label: string;
+  onClick?: () => void;
+}
